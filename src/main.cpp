c8ac09e1098cd925c082fca0618f431810cc78e5
Merlin.rce <racinemerlin@gmail.com>
2026-04-14 19:14:54 +0200

    - Sweep delay from 0ms to 125ms on GPIO 5. - Simulate physical button press by pulling IC pin to LOW. - Results: 10ms identified as the stable minimum threshold.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 #define BTN_UP 5
-void setup() {
-pinMode(BTN_UP, OUTPUT);
+// testing the minimum delay to simulate the button pressed 
+#define minTime 0
+#define maxTime 125
+int delaytime;
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting...");
+  delay(5000);
 }
 
-void loop() {
-digitalWrite(BTN_UP, LOW);
+void loop()
+{
+  Serial.println("===== CYCLE START =====");
+  for (delaytime = minTime; delaytime <= maxTime; delaytime+=1)
+  {
+    Serial.print("Waiting 1000ms... Value: ");
+    Serial.println(delaytime);
+    delay(1000);
+    Serial.println(delaytime);
+    pinMode(BTN_UP, OUTPUT);
+    digitalWrite(BTN_UP, LOW);
+    delay(delaytime);
+    pinMode(BTN_UP, INPUT);
+    delay(delaytime);
+  }
+
+  // Reset for next cycle
+  delaytime = 0;
 }
